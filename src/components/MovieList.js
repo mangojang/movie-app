624dd8694808031953166782de80edd5c97755b7
mangojang994 <mangojang994@gmail.com>
2023-05-05 23:01:16 +0900

    Add : MovieList

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,5 +1,6 @@
 import Component from "../core/component";
 import movieStore from '../store/movie';
+import MovieItem from "./MovieItem";
 
 export default class MovieList extends Component {
     constructor(){
@@ -17,9 +18,8 @@ export default class MovieList extends Component {
         const movieEl = this.el.querySelector('.movies')
         console.log(movieStore.state.movies);
         movieEl.append(
-            movieStore.state.movies.map(movie =>{
-                return movie.Title
-            })
+            ...movieStore.state.movies.map(movie => new MovieItem({ movie }).el
+            )
         )
     }
 }
