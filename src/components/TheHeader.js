e6bb7c07db346fd312518e9a33916e5210076c65
mangojang994 <mangojang994@gmail.com>
2023-05-09 01:39:41 +0900

    Add : Header nav active 적용

--- src/components/TheHeader.js
+++ src/components/TheHeader.js
@@ -21,6 +21,10 @@ export default class TheHeader extends Component {
 				],
 			},
 		});
+
+		window.addEventListener('popstate', () => {
+			this.render();
+		});
 	}
 
 	render() {
@@ -30,9 +34,13 @@ export default class TheHeader extends Component {
                 <ul>
                     ${this.state.menus
 											.map(menu => {
+												const href = menu.href.split('?')[0];
+												const hash = location.hash.split('?')[0];
+												const isActive = href === hash;
 												return /* html */ `
                             <li>
-                                <a href='${menu.href}'>${menu.name}</a>
+                                <a class ='${isActive ? 'active' : ''}'
+                                 href='${menu.href}'>${menu.name}</a>
                             </li>
                         `;
 											})
