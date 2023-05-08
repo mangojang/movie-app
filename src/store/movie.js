6dad6fd0ff8bb5a48e31190cf6cfcc2f80f4e0ff
mangojang994 <mangojang994@gmail.com>
2023-05-08 18:10:13 +0900

    Add : 리스트 더보기

--- src/store/movie.js
+++ src/store/movie.js
@@ -3,20 +3,24 @@ import Store from '../core/store';
 const store = new Store({
 	searchText: '',
 	page: 1,
+	pageMax: 1,
 	movies: [],
+	loading: false,
 });
 
 export default store;
 
 export const searchMovies = async page => {
-	console.log('searchMovies');
+	store.state.loading = true;
+	store.state.page = page;
 	if (page === 1) {
-		store.state.page = 1;
 		store.state.movies = [];
 	}
 	const res = await fetch(
 		`https://www.omdbapi.com/?i=tt3896198&apikey=844b5e77&s=${store.state.searchText}&page=${page}`
 	);
-	const { Search } = await res.json();
+	const { Search, totalResults } = await res.json();
 	store.state.movies = [...store.state.movies, ...Search];
+	store.state.pageMax = Math.ceil(Number(totalResults) / 10);
+	store.state.loading = false;
 };
